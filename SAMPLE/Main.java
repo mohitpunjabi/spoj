00e1df1c50e488880f25dcef707ad8c7b09e8d52
Mohit Punjabi <me@mohitpunjabi.com>
2014-08-02 11:01:28 -0400

    SAMPLE: Added faster I/O methods.

--- SAMPLE/Main.java
+++ SAMPLE/Main.java
@@ -2,14 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	private static final PrintStream out = new PrintStream(new BufferedOutputStream(System.out));
+	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String args[]) throws Exception {		
-		Scanner scanner = new Scanner(System.in);
-
-		int testCases = scanner.nextInt();
-		for(int i = 0; i < testCases; i++) {
-			System.out.println(i + 1);
-		}
+	public static void output(long x, long y) throws Exception {
 	}
 
+	public static void main(String args[]) throws Exception {		
+		int testCases = Integer.parseInt(in.readLine());
+		for(long i = 0; i < testCases; i++) {
+			StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+			long x = Long.parseLong(tokenizer.nextToken()),
+				 y = Long.parseLong(tokenizer.nextToken());
+
+			output(x, y);
+			out.println();
+		}
+
+		out.flush();
+	}
 }
